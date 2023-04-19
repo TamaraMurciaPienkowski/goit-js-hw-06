c19229626f7a2d03fb265bb88ed14a6c861699ec
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-04-19 21:02:58 +0200

    task 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
-const categ = document.querySelector('#categories');
-const categArray = categ.querySelectorAll('li.item');
+const categArray = document.querySelectorAll('li.item');
 console.log(`Number of categories: ${categArray.length}`);
+
+categArray.forEach(item => {
+  const names = item.querySelector('h2').textContent;
+  console.log(`Category: ${names}`);
+  const namesNr = item.querySelectorAll('li').length;
+  console.log(`Elements: ${namesNr}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,9 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ingredientsList = document.querySelector('ul#ingredients');
+for (let i = 0; i < ingredients.length; i += 1) {
+  const li = document.createElement('li');
+  li.textContent = ingredients[i];
+  li.setAttribute('class', 'item');
+  ingredientsList.append(li);
+}
+document.ul.append(ingredientsList);
