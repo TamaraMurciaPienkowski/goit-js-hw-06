a0e95977c20bd28e12e439a627010c5bcf154dd2
TamaraMurciaPienkowski <tamaramurciapienkowski@gmail.com>
2023-04-30 10:18:56 +0200

    corrected task 2 & 5

--- js/task-02.js
+++ js/task-02.js
@@ -1,9 +1,10 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
-const ingredientsList = document.querySelector('ul#ingredients');
+const ingredientsList = document.querySelector('#ingredients');
+const newArray = [];
 for (let i = 0; i < ingredients.length; i += 1) {
   const li = document.createElement('li');
   li.textContent = ingredients[i];
   li.setAttribute('class', 'item');
-  ingredientsList.append(li);
+  newArray.push(li);
 }
-document.ul.ingredients.append(ingredientsList);
+ingredientsList.append(...newArray);
--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,9 @@ const userNameOutput = document.getElementById('name-output');
 const userNameInput = document.getElementById('name-input');
 function inputChange() {
   userNameOutput.textContent = userNameInput.value;
+  if (userNameInput.value === ``) {
+    userNameOutput.textContent = `Anonymous`;
+  }
 }
 
-userNameInput.addEventListener('change', inputChange);
+userNameInput.addEventListener('input', inputChange);
